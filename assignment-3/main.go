ce30c218cab7b1146a1a4472a52ee7cc6e5c514b
Bhuvneshwar Garg <bhuvneshwar.g@coforge.com>
2022-09-26 10:36:32 +0530

    assignment 3 done

--- assignment-3/main.go
+++ assignment-3/main.go
@@ -1,19 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
 // Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 
-// In progress
-
 func main() {
-	n := int64(2)
-
-	binary := strconv.FormatInt(n, 2)
-
-	fmt.Println(binary)
-
+	num := 5
+	result := []int{}
+	for i := 0; i <= num; i++ {
+		pushNum := 0
+		j := i
+		for j > 0 {
+			if j%2 == 1 {
+				pushNum++
+			}
+			j /= 2
+		}
+		result = append(result, pushNum)
+	}
+	fmt.Println(result)
 }
